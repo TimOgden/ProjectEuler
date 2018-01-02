938c9205c649267c8ab4e9d1924775b8b825e85b
Tim Ogden <togden1999@gmail.com>
2018-01-02 14:26:11 -0500

    Not working perfectly

--- Python/largestPrimeFactors.py
+++ Python/largestPrimeFactors.py
@@ -1,9 +1,17 @@
 # My first time creating a python script
 # so I apologize if it's ugly :)
-
+import math
 val = 600851475143
-FermatFactor(n)
 
+
+# n = a^2 - b^2
 def FermatFactor(n):
-	a = math.ceiling(math.sqrt(n))
-	
+	a = math.ceil(math.sqrt(n)) #ceiling round up to next integer
+	b2 = a*a - n
+	while math.sqrt(b2) % 2 != 0:
+		a+=1
+		b2 = a*a - n
+	return a - math.sqrt(b2)
+
+print("Largest prime factor is ")
+print(FermatFactor(val))
